e7eb8f3477d8b78c7923718a9032d02a2b598795
Nancy Li <nancyli1996@gmail.com>
2016-10-27 11:40:51 -0400

    simple yes/no system

--- Q_Answering.py
+++ Q_Answering.py
@@ -12,19 +12,34 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 stemmer = SnowballStemmer("english")
 sentences = []
+wh = ["Who", "What", "When", "Where", "How", "Why"]
 
 
 def main(args):
   doc = open(args[0]).read()
-  q = open(args[1]).read()
-  questions = q.splitlines()
+  qfile = open(args[1]).read()
+  questions = parseQ(qfile.splitlines())
   parseDoc(doc)
 
-  tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-  tfidf_matrix = tfidf.fit_transform(sentences)
+  # tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+  tfidf = TfidfVectorizer(tokenizer=tokenize)
 
-  print tfidf_matrix  
-  # answer(questions)
+  for q in questions:
+    docNum = findDoc(q, tfidf)
+    answer(q, docNum, tfidf)
+  
+  # print tfidf_matrix  
+
+
+def first_word(string):
+  result = ""
+  for c in string:
+    if c == " ":
+      break
+    else:
+      result += c
+
+  return result
 
 
 def tokenize(text):
@@ -35,18 +50,63 @@ def tokenize(text):
   return stems
 
 
+def parseQ(qarray):
+  ret = []
+  for q in qarray:
+    # get the actual question if composite sentence
+    ret.append(q.lower().translate(None, string.punctuation)) 
+  return ret
+
+
 def parseDoc(doc):
   sent = sent_tokenize(doc)
+  print "\nSource:"
   for s in sent:
+    print s
     sentences.append(s.lower().translate(None, string.punctuation))
+  print "\n"
 
 
-# def answer(questions):
-#   for q in questions:
+def findDoc(q, tfidf):
+  maximum = -sys.maxint-1
+  count = 0
+  docNum = 0
+
+  for s in sentences:
+    tfidf_matrix = tfidf.fit_transform([q, s])
+    cosine_sim = ((tfidf_matrix * tfidf_matrix.T).A)[0,1]
+    # print "s: ", s
+    # print "cosine: ", cosine_sim
+
+    if cosine_sim > maximum:
+      docNum = count
+      maximum = cosine_sim
+    count += 1
+
+  return docNum
+
+
+def answer(q, docNum, tfidf):
+  targetSentence = sentences[docNum]
+  print "Question (parsed): ", q
+  print "Target sentence (parsed): ", targetSentence
+  if (first_word(q) not in wh):
+    # yes/no question
+    tfidf.fit_transform([q])
+    fsq = tfidf.get_feature_names()
+    fst = targetSentence.split()
+    if(set(fsq).issubset(set(fst))):
+      print "Answer: Yes. \n"
+    else:
+      print "Answer: No. \n"
+
      # find the most likely sentence using tfidf and cosine similarity
      # yes/no question: match the answer by looking at the words and also POS
      #    answer yes if match, no if no match
      # other: identify keyword with POS and result of supersense tagging and replace
+     # evaluate: ???? train some data set
+     # think of different metrics to get the target sentence
+     # is the grass not red?
 
 
 if __name__ == '__main__':
