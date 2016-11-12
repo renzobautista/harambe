cea2f4709e8405f6c083bc0f39659202e1a0f7ba
nancyl-ol <nancyli1996@gmail.com>
2016-11-12 17:50:59 -0500

    Update SentenceParser.py

--- SentenceParser.py
+++ SentenceParser.py
@@ -1,11 +1,12 @@
 from bllipparser import RerankingParser
+from nltk.tree import Tree
 
 class SentenceParser():
     rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
 
     @staticmethod
     def parse(sentence):
-        return SentenceParser.rrp.simple_parse(sentence)
+        return Tree.fromstring(SentenceParser.rrp.simple_parse(sentence))
 
     @staticmethod
     def tree_parse(sentence):
