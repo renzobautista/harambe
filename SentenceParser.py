3b330b59793bcc28f80a0c3bf37e63081bffc0bc
Renzo Bautista <renzobautista2@gmail.com>
2016-11-17 15:30:34 -0500

    set verbose to false

--- SentenceParser.py
+++ SentenceParser.py
@@ -2,7 +2,7 @@ from bllipparser import RerankingParser
 from nltk.tree import Tree
 
 class SentenceParser():
-    rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
+    rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=False)
 
     @staticmethod
     def parse(sentence):
